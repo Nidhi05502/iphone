9820c14a987cf5287405aa2dd8133d5c5bb5a97f
Nidhi <nidhisharma5m2000@.com>
2025-06-02 12:14:50 +0530

    second commit

---
+++ JavaScript/code.js
@@ -0,0 +1,55 @@
+function dropDown(){
+  const content=document.getElementById("myDropdown");
+  if(content.style.display == "block"){
+    content.style.display ="none";
+  }
+  else{
+    content.style.display="block";
+  }
+}
+window.onclick=function(event){
+  if(!event.target.matches('.dropbtn')){
+    document.getElementById('myDropdown').style.dispaly="none";
+  }
+}
+
+function emidropbtn(){
+  const cont=document.getElementById("dropdown-emi");
+  if(cont.style.display == "block"){
+    cont.style.display ="none";
+  }
+  else{
+    cont.style.display="block";
+  }
+}
+window.onclick=function(event){
+  if(!event.target.matches('.drpbtn')){
+    document.getElementById('dropdown-emi').style.dispaly="none";
+  }
+}
+const tabBody=document.querySelector(".tab-body");
+const tabs=document.querySelector(".tabs");
+const tabButtons=tabs.querySelectorAll("[role='tab']");
+const tabPanels=Array.from(tabBody.querySelectorAll("[role='tabpanel']"));
+ 
+function tabclickhandler(event){
+ 
+  tabPanels.forEach(panel=>(panel.hidden = true));
+ 
+  tabButtons.forEach(button=>button.setAttribute("aria-selected",false));
+ 
+  event.currentTarget.setAttribute("aria-selected",true);
+ 
+  const{id}=event.currentTarget;
+  console.log(id);
+ 
+  const tabpanel=tabBody.querySelector(`[aria-labelledby="${id}"]`);
+  tabpanel.hidden=false;
+ 
+}
+ 
+tabButtons.forEach(button => button.addEventListener("click",tabclickhandler));
+
+
+
+ 
